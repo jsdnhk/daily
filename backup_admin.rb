e3aa3402b5a1c7591d0ecba8b2d5ef7bcfa0b5a3
David N <ngaida@oocl.com>
2018-11-08 17:53:51 +0800

    housekeep function added!

--- backup_admin.rb
+++ backup_admin.rb
@@ -10,9 +10,11 @@
 require 'fileutils'
 require 'date'
 
-module BACKUP_ADMIN
+module BackupAdmin
+  DateFormat="%Y%m%d"
+
   protected
-  def isValidDirPath(dir_path)
+  def is_valid_dir_path?(dir_path)
     bool_return = false
     if Dir.exists?(dir_path)
       bool_return = true
@@ -27,7 +29,7 @@ module BACKUP_ADMIN
     return bool_return
   end
 
-  def getDateTime(str_date, str_format = "%Y%m%d")
+  def get_date_time(str_date, str_format = BackupAdmin::DateFormat)
     obj_return = nil
     begin
       t_now = Time.now
@@ -40,20 +42,23 @@ module BACKUP_ADMIN
   end
 end
 
+class << $stdout
+end
+
 class CPBKP
-  include BACKUP_ADMIN
+  include BackupAdmin
   def cpbkp()
-    if !isValidDirPath($copy_src_folder)
+    if !is_valid_dir_path?($copy_src_folder)
       puts("the src folder [#{$copy_src_folder}] is not a valid folder path")
       exit(false)
-    elsif !isValidDirPath($copy_dest_path)
+    elsif !is_valid_dir_path?($copy_dest_path)
       puts("the dest folder [#{$copy_dest_path}] is not a valid folder path")
       exit(false)
     end
 
     puts("copy the files from the folder [#{$copy_src_folder}] to the directory [#{$copy_dest_path}]...")
 
-    if backupPlusCopyFiles()
+    if backup_plus_copy_files?()
       puts("all files related are backup and copied successfully!!")
       exit(true)
     else
@@ -63,7 +68,7 @@ class CPBKP
   end
 
   private
-  def backupPlusCopyFiles()
+  def backup_plus_copy_files?()
     bool_return = true
     begin
       #initialization
@@ -72,13 +77,18 @@ class CPBKP
       $copy_src_files_pattern = File.join("#{$copy_src_folder}","**","*")
       $copy_src_files = Dir.glob($copy_src_files_pattern).select{|filename| File.file?(filename)}
       $copy_src_files.each do |src_file|
-        obj_result = backupPlusCopyOneFile(src_file)
-        if obj_result
-          $stdout.puts("[#{src_file}] are made to:") if $is_verbose && !src_file.nil?
-          $stdout.puts("the backup file: [#{obj_result[0]}]") if $is_verbose && !obj_result[0].nil?
-          $stdout.puts("the desc file: [#{obj_result[1]}]") if $is_verbose && !obj_result[1].nil?
-        else
-          $stdout.puts("the file [#{src_file}] cannot successfully backuped and copied.") if $is_verbose
+        int_result, obj_result = backup_plus_copy_one_file(src_file)
+        case int_result
+          when 1
+            $stdout.puts("the file [#{src_file}] have already existed and instincted, won't copy.") if $is_verbose
+          when 0
+            $stdout.puts("[#{src_file}] are made to:") if $is_verbose && !src_file.nil?
+            $stdout.puts("the backup file: [#{obj_result[0]}]") if $is_verbose && !obj_result[0].nil?
+            $stdout.puts("the desc file: [#{obj_result[1]}]") if $is_verbose && !obj_result[1].nil?
+          when -1
+            $stdout.puts("the file [#{src_file}] cannot successfully backuped and copied.") if $is_verbose
+          else
+            raise("Unknown output from [#{src_file}]")
         end
       end
     rescue
@@ -90,18 +100,26 @@ class CPBKP
   end
 
   private
-  def backupPlusCopyOneFile(src_file)
+  def backup_plus_copy_one_file(src_file)
+    int_return = nil
     obj_return = nil
     filepath_backuped = nil
     filepath_copied = nil
     begin
-      str_date = $t_current.strftime("%Y%m%d")
+      str_date = $t_current.strftime(DateFormat)
       str_date = !$backup_date.empty? && $backup_date =~ /^\d{8}$/ ? $backup_date : str_date
       src_filename = File.basename(src_file)
       src_relative_path = src_file.sub(/#{$copy_src_folder}/,'')
       dest_file_absolute_path = File.join($copy_dest_path, src_relative_path)
       dest_backup_absolute_path = File.join(File.dirname(dest_file_absolute_path),"#{src_filename}.#{str_date}")
       backup_num = 0
+
+      #check the diff., skipped when there are no diff.
+      cmd_diff = %x(diff -s #{src_file} #{dest_file_absolute_path})
+      if ($?.success?)
+        int_return = 1
+        raise("the src and dest are the same")
+      end
       #backup before copying
       st_file = nil
       default_uid = nil
@@ -113,9 +131,9 @@ class CPBKP
         default_gid = st_file.gid
         default_mode = st_file.mode
         while filepath_backuped.nil?
-          dest_backup_path = dest_backup_absolute_path + ( backup_num > 0 ? "-#{backup_num.to_s}" : "" )
-          backup_num++
-          next if File.exists?(dest_backup_path)
+          dest_backup_path = dest_backup_absolute_path + ( backup_num > 0 ? ".#{backup_num.to_s}" : "" )
+          backup_num = backup_num + 1
+          next if File.exist?(dest_backup_path)
           FileUtils.move(dest_file_absolute_path, dest_backup_path, :force => false)
           filepath_backuped = dest_backup_path
         end
@@ -123,7 +141,10 @@ class CPBKP
       end
       #then copy
       if File.file?(src_file)
-        raise("dest folder cannot be made, the copying action cancelled.") if !isValidDirPath(File.dirname(dest_file_absolute_path))
+        if !is_valid_dir_path?(File.dirname(dest_file_absolute_path))
+          int_return = -1
+          raise("dest folder cannot be made, the copying action cancelled.")
+        end
         #FileUtils.cp(src_file, dest_file_absolute_path, :preserve => true)
         FileUtils.cp(src_file, dest_file_absolute_path, preserve: true)
         if !st_file.nil?
@@ -133,68 +154,109 @@ class CPBKP
         filepath_copied = dest_file_absolute_path
         $stdout.puts("the dest file [#{filepath_copied}] is copied from the src file[#{src_file}].") if $is_verbose
       else
+        int_return = -1
         raise("the src file #{src_file} is not existed.")
       end
     rescue
       $stderr.puts "An error occurred when copying [#{src_file}]: #{$!}"
       filepath_backuped = nil
       filepath_copied = nil
+      obj_return = nil
     ensure
       if filepath_backuped != nil || filepath_copied != nil
+        int_return = 0
         obj_return = [filepath_backuped, filepath_copied]
-      else
-        obj_return = nil
       end
     end
-    return obj_return
+    return int_return, obj_return
   end
 end
 
 class CLR
-  include BACKUP_ADMIN
+  include BackupAdmin
+end
+
+class HK
+  include BackupAdmin
 
   def initialize(backup_date)
-    @date_backup_date = getDateTime(backup_date)
+    @date_backup_date = get_date_time(backup_date)
     @str_backup_date = backup_date
   end
 
-  def clr()
-    $stdout.puts("clear the files from the folder [#{$copy_src_folder}] with the backup date [#{$backup_date}]...")
-    if clearBackupFiles()
-      $stdout.puts("all files related are backup and copied successfully!!")
+  public
+  def hk()
+    $stdout.puts("clear the files from the folder [#{$copy_src_folder}] to date [#{$backup_date}]...")
+    if clear_backup_files?()
+      $stdout.puts("all files related are cleared successfully!!")
       exit(true)
     else
-      $stderr.puts("the clr processes are failed with errors?! Please verify and try again.")
+      $stderr.puts("the hk processes are failed with errors?! Please verify and try again.")
       exit(false)
     end
   end
 
   private
-  def clearBackupFiles()
+  def clear_backup_files?()
+    bool_return = true
+    begin
+      raise("no correct datetime input") unless @date_backup_date
 
+      find_result_src = %x(find #{$copy_dest_path} -type f).split("\n")
+      find_result_dest = %x(find #{$copy_dest_path} -type f).split("\n")
+      find_result = find_result_src + find_result_dest
+      find_result.each do |item|
+        findfile_filename = File.basename(item)
+        str_date_findfile = findfile_filename.split('.').reverse.inject(nil) do |memo, obj; var|
+          var = /^[0-9]{8}/.match(obj)
+          if var && !memo
+            break var.to_s
+          else
+            next memo
+          end
+        end
+
+        t_date_findfile = get_date_time(str_date_findfile, DateFormat)
+        next unless t_date_findfile && (@date_backup_date - t_date_findfile >= 0)
+        FileUtils.rm_f([item])
+        $stdout.puts("the backup file [#{item}] is removed successfully") if $is_verbose
+      end
+    rescue
+      $stderr.puts "An error occurred when copying backup files to [#{@str_backup_date}]: #{$!}"
+      bool_return = false
+    ensure
+    end
+    return bool_return
   end
 end
 
 $t_current=Time.now
+$str_t_current=$t_current.strftime(BackupAdmin::DateFormat)
 $is_verbose=true
-$action=ARGV[0].to_s
-$copy_src_folder=ARGV[1].to_s
-$copy_dest_path=ARGV[2].to_s
-$backup_date=ARGV[3].to_s
+$action=ARGV[0].to_s.strip.downcase
+$copy_src_folder=ARGV[1].to_s.strip
+$copy_dest_path=ARGV[2].to_s.strip
+$backup_date=ARGV[3].to_s.strip
+$action.freeze
+$copy_src_folder.freeze
+$copy_dest_path.freeze
+$backup_date.freeze
 
 if ARGV.length < 3
-  $stderr.puts("the script requires two input parm.")
+  $stderr.puts("the script requires at least three input parm.")
   $stderr.puts("./backup_admin.rb {{ action }} {{ copy_src_folder }} {{ copy_dest_path }} {{ backup_date }}")
   exit(false)
 end
 
-case $action.to_s.downcase!
+case $action
   when 'cpbkp'
     CPBKP.new.cpbkp()
   when 'clr'
     CLR.new($backup_date).clr()
   when 'hk'
+    HK.new($backup_date).hk()
   when 'rc'
   else
-    fail("")
+    $stderr.puts("unavailable action type [#{$action}]! please input the arguments values again.")
+    exit(false)
 end
