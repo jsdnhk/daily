915ea720490dc34921fefb6ba59d52f2b00135a0
David N <david@keep.edu.hk>
2020-02-23 23:13:55 +0800

    categorized scripts/tools

--- bash/translate_dirfiles_chinese.sh
+++ bash/translate_dirfiles_chinese.sh
@@ -4,7 +4,9 @@ set -eu
 set -o pipefail
 
 # Purpose: convert file/dir name + content to other form of Chinese (zh-cn to zh-tw as default)
-# Prequisites: opencc installed
+# Prequisites:
+# 1) opencc
+# 2) moreutils
 
 # if want trad to simp chinese, use 't2s.json' instead
 readonly conf_opencc='s2t.json'
@@ -20,7 +22,7 @@ elif [ ! -d "${target_dir}" ]; then
   exit -2
 fi
 
-dir_depth=$(find ${target_dir} -type d -not -path '*/\.*' -printf '%d\n' | sort -rn | head -1)
+dir_depth=$(find ${target_dir} -type d -not -path "*/\.*" | awk -F"/" 'NF > max {max = NF} END {print max}')
 for depth in $(seq 1 ${dir_depth}); do
   for subdir in $(find ${target_dir} -mindepth ${depth} -maxdepth ${depth} -type d -not -path '*/\.*'); do    # ignore hidden files/dirs
     subdir_new=$(echo -n ${subdir} | opencc -c ${conf_opencc})
@@ -30,14 +32,12 @@ for depth in $(seq 1 ${dir_depth}); do
   done
 done
 
-for file in $(find ${target_dir} -type f -not -path '*/\.*'); do  
-  # content_new=$(opencc -i ${file} -c ${conf_opencc})
+for file in $(find ${target_dir} -type f -not -path '*/\.*'); do
   filename_new=$(echo -n ${file} | opencc -c ${conf_opencc})
   if [ ! -e ${filename_new} ];then
     mv -f ${file} ${filename_new}
   fi
-  # echo -n ${content_new} > ${filename_new}
-  opencc -i ${file} -c ${conf_opencc} | sponge ${filename_new}
+  opencc -i ${filename_new} -c ${conf_opencc} | sponge ${filename_new}
 done
 
 echo "All the file inside the dir. inside ${target_dir} is converted to Sim.Chinese to Trad.Chinese!"
